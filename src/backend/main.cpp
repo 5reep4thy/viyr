6be8015cca1efe4730fdd9889a1d696f30dea68d
Sreepathy Jayanand <sreepathy0101@gmail.com>
2024-11-10 11:57:29 +0530

    Logging added

--- src/backend/main.cpp
+++ src/backend/main.cpp
@@ -3,11 +3,25 @@
 #include <iostream>
 #include <boost/algorithm/string/case_conv.hpp>
 using json = nlohmann::json;
+#include <boost/log/trivial.hpp>
+
 int main() {
     std::ifstream f("files/example.json");
-    json data = json::parse(f);
-    std::string strData = data["title"];
-    boost::algorithm::to_lower(strData);
-    std::cout << strData << "\n";
+    json data;
+
+    try {
+        data = json::parse(f);
+        std::string strData = data["title"];
+        boost::algorithm::to_lower(strData);
+        std::cout << strData << "\n";
+        std::vector<int> x;
+        int h = 12;
+        x.push_back(h);
+    }
+
+    catch (json::exception) {
+        BOOST_LOG_TRIVIAL(error) << "Something went wrong parsing the JSON file";
+    }
+
     return 0;
 }
